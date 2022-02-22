f8ab7afd59ac9ba3caa58b7221176fe4407c8721
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-02-22 12:30:59 -0300

    Contando quantos números pares e ímpares existentes entre 1 e 10 bem como a soma deles usando a instrução while

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,18 @@
-# Contando de 1 até 10 usando a instrução for e a função range  
+# Contando quantos números pares e ímpares existentes entre 1 e 10 bem como a soma deles usando a instrução while 
 
 n = 1
-for n in range (1,11):
-    print(n)
+P = 0
+I = 0
+while n <= 10:
+    a = int(input())
+    n += 1
+    if a % 2 == 0:
+        a = P
+        P += 1
+    else: 
+        a = I
+        I += 1
+print("A quantidade de números pares: ", P)
+print("A quantidade de números ímpares: ", I)
+
 
