12d41d258519a786bc2935412c9361e2e9310b21
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-02-22 12:21:39 -0300

    Contando de 1 até 10 usando a instrução for e a função range

--- exercicios.py
+++ exercicios.py
@@ -1,7 +1,6 @@
-# Contando de 1 até 10 usando a instrução while 
+# Contando de 1 até 10 usando a instrução for e a função range  
 
 n = 1
-while n <= 10:
+for n in range (1,11):
     print(n)
-    n += 1
 
