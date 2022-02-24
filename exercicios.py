a6c494b3a1be3122b847c966c136714533394807
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-02-24 14:14:35 -0300

    Calculando as raízes de uma equação do 2° Grau usando o módulo math

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,12 @@
-# Calcular as raízes de uma equação do 2° Grau
+# Calcular as raízes de uma equação do 2° Grau usando o módulo math 
+
+import math
 
 a = int(input("Coeficiente de a: "))
 b = int(input("Coeficiente de b: "))
 c = int(input("Coeficiente de c: "))
 delta = (b**2 - 4*a*c)
-x1 = (-b + delta**(1/2)) / (2*a)
-x2 = (-b - delta**(1/2)) / (2*a)
+x1 = (-b + math.sqrt(delta)) / (2*a)
+x2 = (-b - math.sqrt(delta)) / (2*a)
 
 print("Raízes: ",x1," e ",x2)
