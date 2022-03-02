e03a3a1517d8a7cf1e1b372f76e1e51c7af69cb5
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-03-02 15:37:54 -0300

    Escrevendo um programa Python para inserir um elemento no início de um determinado DicionárioOrdenado

--- exercicios.py
+++ exercicios.py
@@ -1,19 +1,4 @@
-# Criando 3 conjuntos conforme estrutura a seguir:
-setx = set(["apple", "mango"])
-sety = set(["mango", "orange"])
-setz = set(["mango"])
-
-# a)Fazendo a união dos três conjuntos e imprimindo o resultado
-união = setx | sety | setz
-print("A união dos três conjunto: ", união)
-
-# b)Verifiquando quais os elementos comuns do conjunto setx e sety e imprima o resultado
-intercessão = setx & sety
-print("Os elementos comuns do conjunto setx e sety: ", intercessão)
-
-# c)Verifiquando se o conjunto setx é subconjunto do conjunto sety e setz utilizando
-print("O conjunto setx é subconjunto sety: ", setx.issubset(sety))
-print("O conjunto setx é subconjunto setz: ",setx.issubset(setz))
-
-# d)Verifiquando quais elementos do conjunto setx não existem em sety
-print("Quais elementos do conjunto setx não existem em sety: ", setx.difference(sety))
+# Escrevendo um programa Python para inserir um elemento no início de um determinado DicionárioOrdenado
+dicionario = ([('color1', 'Red'), ('color2', 'Green'), ('color3', 'Blue')])
+dicionario.insert(0, [('color4', 'Orange')])
+print(dicionario)
