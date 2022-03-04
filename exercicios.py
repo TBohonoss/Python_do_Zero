1e8a98147d8368e55d5d99bf3f1921834ec7739c
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-03-04 14:43:07 -0300

    Usando uma fila e uma pilha

--- exercicios.py
+++ exercicios.py
@@ -1,16 +1,15 @@
-# Entrando com a mensagem e com o uso de módulos e funções
-s = input("Digite a mensagem ser impressa: ")
+# Usando uma fila e uma pilha
+letras = []
+numeros = []
+digitos = [0,1,2,3,4,5,6,7,8,9]
+lista = ['E',7,'T',8,'K',5,'C',0,9,'F']
 
-def entrada():
-  return s
-print(s)
-
-# Entrando com os valores de dois multiplicadores e exibindo o resultado na tela
-Valor1 = int(input('Digite o 1º valor: '))
-Valor2 = int(input('Digite o 2º valor: '))  
-
-def calcular_multiplicação (Valor1, Valor2):
-    multiplicação = Valor1 * Valor2
-    return print('A mulptiplicação dos valores é: ',multiplicação)
-
-calcular_multiplicação (Valor1, Valor2)
+for e in lista:
+    if e in digitos:
+        numeros.append(e)
+    else:
+        letras.append(e)
+letras.sort()
+numeros.sort()
+total = letras + numeros
+print(total)
