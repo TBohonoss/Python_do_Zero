09fec6724a9509c0664d8d17fcb5661f51d7f317
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-03-04 14:41:55 -0300

    Entrando com a mensagem e valores na função

--- exercicios.py
+++ exercicios.py
@@ -1,9 +1,16 @@
-# Chamando a função duas vezes, passando o valor como um argumento
-def do_twice(func, arg):
-    func(arg)
-    func(arg)
+# Entrando com a mensagem e com o uso de módulos e funções
+s = input("Digite a mensagem ser impressa: ")
 
-def print_valor(arg):
-    print(arg)
+def entrada():
+  return s
+print(s)
 
-do_twice(print, 'spam')
+# Entrando com os valores de dois multiplicadores e exibindo o resultado na tela
+Valor1 = int(input('Digite o 1º valor: '))
+Valor2 = int(input('Digite o 2º valor: '))  
+
+def calcular_multiplicação (Valor1, Valor2):
+    multiplicação = Valor1 * Valor2
+    return print('A mulptiplicação dos valores é: ',multiplicação)
+
+calcular_multiplicação (Valor1, Valor2)
