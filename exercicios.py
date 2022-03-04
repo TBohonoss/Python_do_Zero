f72f97734a97a56676d44258d56ac32e78e3728a
Thiago Bohonos <94501196+TBohonoss@users.noreply.github.com>
2022-03-04 14:32:43 -0300

    Escrevendo uma função chamada right_justify

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,13 @@
-# Escrevendo um programa Python para inserir um elemento no início de um determinado DicionárioOrdenado
-dicionario = ([('color1', 'Red'), ('color2', 'Green'), ('color3', 'Blue')])
-dicionario.insert(0, [('color4', 'Orange')])
-print(dicionario)
+# Escrevendo uma função chamada right_justify, que receba uma string chamada s como parâmetro 
+# e exiba a string com espaços suficientes à frente para que a última letra da string esteja na coluna 70 da tela:
+string = "s"
+
+def right_justify(string):
+    i = 1
+    j = 70 - len(string)
+    while i<= j:
+        string = " " + string
+        i += 1
+    return print(string)
+
+right_justify(string)
